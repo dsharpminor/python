a0c75eac4f2d3b0ba8ddb0ed60c8ae53b861976d
Ametrien <anastasia.ivanova@ati.utm.md>
2022-03-06 23:37:16 +0100

    Updated

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Python
 
 I am using multiple sources to learn how to code in Python, and each of them has got a designated folder.
+#### Done:
+* ✅ Basics - [Giraffe Academy - Mike's Basic Python Course](https://youtu.be/rfscVS0vtbw)
+* ✅ Intermediate / Advanced - [Patrick Loeber's Advanced Python course](https://youtu.be/HGOBQPFzWKo)
+#### In progress:
+* OOP - [Jim Shaped's OOP course](https://youtu.be/Ej_02ICOIgs)
+#### To do:
+* Full Python Course (recap) - [Jim Shaped's full Python course](https://youtu.be/m0LdKZ-prto)
 
-* Basics - [Giraffe Academy](https://youtu.be/rfscVS0vtbw)
-* Intermediate - [Patrick Loeber's course](https://youtu.be/HGOBQPFzWKo)
-* OOP - [jimdeops19's course](https://youtu.be/Ej_02ICOIgs)
---
+++ YouTube /freeCodeCamp/OOP/01_classes.py
@@ -0,0 +1,77 @@
+"""
+Assert statements check if there's a match between code and your expectations
+1. Validates the arguments that we receive
+2. Catch up the bugs asap before we go forward
+
+Instance attributes are normal attributes.
+Class attributes are common for every item.
+"""
+
+
+class Item:
+
+    pay_rate = 0.8  # The pay rate after 20% discount
+    all = []
+
+    def __init__(self, name: str, price: float, quantity: int):
+        print(f"An instance named {name} is created.")
+
+        # Run validations to the received arguments
+        assert price > 0, f"Price {price} is not greater than zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
+
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        # Actions to execute
+        Item.all.append(self)
+
+
+    def all_attributes(self):
+        return self.name, self.price, self.quantity
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+
+    def apply_discount(self):
+        # overwrite an attribute
+        # self.price = int(self.price * Item.pay_rate)  # no matter what - the discount is from the class level
+        self.price = int(self.price * self.pay_rate)    # pay_rate from instance level. nothing? then class level
+
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
+
+
+item1 = Item("Phone", 15100, 4)
+item2 = Item("MacBook", 45000, 1)
+print(item1.all_attributes())
+print(item1.calculate_total_price())
+
+print(Item.pay_rate)  # print a Class attribute
+print(item1.pay_rate)
+
+print(Item.__dict__)  # all Class level attributes
+print(item1.__dict__)  # all instance level attributes
+
+print(f"{item1.price} before discount")
+item1.apply_discount()
+print(f"{item1.price} after discount")
+item2.pay_rate = 0.95  # override the value
+
+print(f"{item2.price} before discount")
+item2.apply_discount()
+print(f"{item2.price} after discount")
+
+
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
+
+print(Item.all)  # 5 objects -> def __repr__ -> 5 readable objects
+
+for i in Item.all:
+    print(i.name)
+
+# https://youtu.be/Ej_02ICOIgs?t=3104
