c059355c4c03457410cf27ba290b6f66c13a68e6
Ametrien <anastasia.ivanova@ati.utm.md>
2022-03-09 00:59:23 +0100

    TicTacToe (working demo)

--- Projects/TicTacToe/player.py
+++ Projects/TicTacToe/player.py
@@ -38,15 +38,16 @@ class Player:
 
         # print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in b.field]))
 
-    # def win(self):
-    #     if (self.b.field[0][0] == self.b.field[0][1] == self.b.field[0][2] == self.symbol or
-    #             self.b.field[1][0] == self.b.field[1][1] == self.b.field[1][2] == self.symbol or
-    #             self.b.field[2][0] == self.b.field[2][1] == self.b.field[2][2] == self.symbol or
-    #             self.b.field[0][0] == self.b.field[1][0] == self.b.field[2][0] == self.symbol or
-    #             self.b.field[0][1] == self.b.field[1][1] == self.b.field[2][1] == self.symbol or
-    #             self.b.field[0][2] == self.b.field[1][2] == self.b.field[2][2] == self.symbol or
-    #             self.b.field[0][0] == self.b.field[1][1] == self.b.field[2][2] == self.symbol or
-    #             self.b.field[0][2] == self.b.field[1][1] == self.b.field[2][0] == self.symbol):
-    #         print("Player 1 won. I love you Prohor")
-
-            # return True
+    def win(self, b):
+        if (b[0][0] == b[0][1] == b[0][2] == self.symbol or
+                b[1][0] == b[1][1] == b[1][2] == self.symbol or
+                b[2][0] == b[2][1] == b[2][2] == self.symbol or
+                b[0][0] == b[1][0] == b[2][0] == self.symbol or
+                b[0][1] == b[1][1] == b[2][1] == self.symbol or
+                b[0][2] == b[1][2] == b[2][2] == self.symbol or
+                b[0][0] == b[1][1] == b[2][2] == self.symbol or
+                b[0][2] == b[1][1] == b[2][0] == self.symbol):
+            # print("Player 1 won. I love you Prohor")
+            return True
+        else:
+            return False
